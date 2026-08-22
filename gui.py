1f0a17d96be7889492e4961807404ce5dee70810
seantran-dev <stran1019@gmail.com>
2026-08-22 10:42:14 -0500

    update

--- gui.py
+++ gui.py
@@ -916,6 +916,11 @@ class GUI:
 
     def display_my_message(self, message, save=True):
 
+        print("SAVING SENT MESSAGE")
+        print("current_contact:", self.current_contact)
+        print("user_id:", self.user_id)
+        print("database:", self.database)
+
         if not self.current_contact:
             return
 
