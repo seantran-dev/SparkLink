ed27404d8a7e211c8c10dfe969930fc26f02e83a
seantran-dev <stran1019@gmail.com>
2026-09-16 18:11:43 -0500

    test

--- gui.py
+++ gui.py
@@ -1520,20 +1520,12 @@ class GUI:
         if self.current_contact == user_id:
             self.current_contact = None
             self.chat_title.setText("")
+            self.chat.clear()
             self.message_box.clear()
             self.message_box.setEnabled(False)
             self.send_button.setEnabled(False)
             self.file_button.setEnabled(False)
             self.typing_label.hide()
-
-            # Reset the chat area
-            while self.chat_layout.count():
-                item = self.chat_layout.takeAt(0)
-                widget = item.widget()
-                if widget:
-                    widget.deleteLater()
-
-            self.chat.scrollToBottom()
             
     def handle_discovered_contact(
         self,
